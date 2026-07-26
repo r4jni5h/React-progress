e742fa22566c154da2963baaf030fb13378d8d21
r4jni5h <rajnishyadav6658@gmail.com>
2026-07-26 16:23:40 +0530

    Learn CSS module

---
+++ CSS05/src/Components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './header.module.css'
+
+const Header = () => {
+  return (
+    <div className={styles.header}>
+        <h1>Hello My Name is Rajnish</h1>
+
+    </div>
+  )
+}
+
+export default Header
---
+++ CSS05/src/Components/buttons/Button.jsx
@@ -0,0 +1,13 @@
+ import React from 'react'
+ import styles from './buttons.module.css'
+ 
+ const Button = () => {
+   return (
+     <div className={styles.btn}>
+      <h1>Yadav Info Tech</h1>
+      <button>Apply Now</button>
+     </div>
+   )
+ }
+ 
+ export default Button
